2b8d9fba6de09a960870acd185f2627ee987c265
Natasa <moj_email@gmail.com>
2018-09-17 23:21:18 +0200

    seeder

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Gallery;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-       
+        'body' => $faker->text(300),
+        'user_id' => User::all()->random()->id,
+        'gallery_id' => Gallery::all()->random()->id
     ];
 });
--- database/factories/GalleryFactory.php
+++ database/factories/GalleryFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Gallery::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(3),
         'description' => $faker->text(200),
-        'user_id' => $faker->numberBetween(1, 10)
+        'user_id' =>  User::all()->random()->id
 
     ];
 });
